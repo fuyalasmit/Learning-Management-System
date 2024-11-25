4a526fe22aade07c90cfd8b911cda6e33b0ac94a
Asmit Phuyal <asmitphuyal4040@gmail.com>
2024-11-25 18:16:43 +0545

    first

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+
+const App = () => {
+  return (
+    <div>
+      <Button>Lets build LMS ❤</Button>
+    </div>
+  )
+}
+
+export default App
