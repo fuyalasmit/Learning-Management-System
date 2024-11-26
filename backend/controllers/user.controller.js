eeace9608690a149d2d0b330507491c9c344ef55
Asmit Phuyal <asmitphuyal4040@gmail.com>
2024-11-26 15:42:00 +0545

    api tested

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,4 +1,6 @@
-import { User } from '../models/user.model';
+import { User } from '../models/user.model.js';
+import bcrypt from 'bcrypt';
+import { generateToken } from '../utils/generate.token.js';
 
 export const register = async (req, res) => {
   try {
@@ -10,16 +12,56 @@ export const register = async (req, res) => {
     }
     const user = await User.findOne({ email });
     if (user) {
+      return res.status(400).json({
+        success: false,
+        message: 'User already exists with this email.',
+      });
+    }
+
+    const hashedPassword = bcrypt.hash(password, 10, (err, hash) => {});
+    await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    return res
+      .status(201)
+      .json({ success: true, message: 'Account created successfully' });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ success: false, message: 'Failed to register' });
+    console.log(error.message);
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
       return res
         .status(400)
-        .json({
-          success: false, message: 'User already exists with this email.',
-        });
+        .json({ success: false, message: 'All fields are required.' });
     }
-    await User.create({
-        name,
-        email,
-        password
-    })
-  } catch (error) {}
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: 'Incorrect email or password' });
+    }
+    const isPasswordMatch = bcrypt.compare(
+      password,
+      user.password,
+      function (err, result) {}
+    );
+    if (!isPasswordMatch) {
+      return res
+        .status(400)
+        .json({ success: false, message: 'Incorrect email or password' });
+    }
+    generateToken(res,user,`Welcome back ${user.name}`);
+  } catch (error) {
+    return res.status(500).json({ success: false, message: 'Failed to login' });
+    console.log(error.message);
+  }
 };
--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,33 @@
-import express from "express";
-import dotenv from "dotenv";
-import connectDB from "./config/db.js";
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+import userRoute from './routes/user.route.js';
+import cookieParser from "cookie-parser"
+import cors from "cors";
 
 dotenv.config();
+
+//db connection
 connectDB();
 const app = express();
 
-const PORT = process.env.PORT || 3000 
+const PORT = process.env.PORT || 3000;
 
-app.get("/",(req,res)=>{
-    res.send("hi")
-})
+//default middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    origin:"http://localhost:3000",
+    credentials:true
+}));
 
-app.listen(PORT,()=>{
-    console.log(`Server listening at port ${PORT}`);
-})
+//api
+app.use('/api/v1/user', userRoute);
+
+app.get('/helloworld', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening at port ${PORT}`);
+});
