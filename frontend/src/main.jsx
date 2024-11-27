7d743542355f66fb666499b5fdf0dee011ab9b01
Asmit Phuyal <asmitphuyal4040@gmail.com>
2024-11-27 19:47:12 +0545

    configured store

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { Provider } from 'react-redux';
+import { appStore } from './store/store';
 
 createRoot(document.getElementById('root')).render(
+  <Provider store={appStore}>
     <App />
-)
+  </Provider>
+);
