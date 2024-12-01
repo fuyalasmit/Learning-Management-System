69b19c4a0452f3ad726cbff6be643c4242f78891
Asmit Phuyal <asmitphuyal4040@gmail.com>
2024-12-01 22:52:13 +0545

    user icon added

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,13 +1,61 @@
-import {  School2 } from 'lucide-react';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu';
+import { School2 } from 'lucide-react';
 import React from 'react';
+import { Button } from './ui/button';
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 
 const Navbar = () => {
+  const user = false;
   return (
     <div className="h-16 dark:bg-[#020817] bg-white border-b dark:border-b-gray-800 border-b-gray-200 fixed top-0 left-0 right-0 duration-300 z-10">
-      <div className="max-w-7xl mx-auto hidden md:flex justify-between items-center gap-10 h-full p-3"> 
-        <div className='flex items-center gap-2' >
+      {/* for desktop  */}
+      <div className="max-w-7xl mx-auto hidden md:flex justify-between items-center gap-10 h-full p-3">
+        <div className="flex items-center gap-2">
           <School2 size={'30'} />
-          <h1 className="hidden md:block font-extrabold text-2xl">      E-Learning  </h1>
+          <h1 className="hidden md:block font-extrabold text-2xl">
+            {' '}
+            E-Learning{' '}
+          </h1>
+        </div>
+        {/* user icons and dark mode button */}
+        <div>
+          {user ? (
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Avatar>
+                  <AvatarImage src="https://github.com/shadcn.png" />
+                  <AvatarFallback>ACC</AvatarFallback>
+                </Avatar>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-52">
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                  <DropdownMenuItem>My Learning</DropdownMenuItem>
+                  <DropdownMenuItem>Edit Profile</DropdownMenuItem>
+                  <DropdownMenuItem>Log Out</DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                  <DropdownMenuItem>Dashboard</DropdownMenuItem>
+                </DropdownMenuGroup>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          ) : (
+            <div className="flex items-center gap-2">
+              <Button variant="outline">Log In</Button>
+              <Button>Sign Up</Button>
+            </div>
+          )}
+          {/* <DarkMode/> */}
         </div>
       </div>
     </div>
